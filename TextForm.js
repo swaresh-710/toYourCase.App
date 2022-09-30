475aeedadf186f5cb9e6e9a3c525749e2f83a850
swaresh binaykiya <72277918+swaresh-710@users.noreply.github.com>
2022-09-30 17:52:19 +0530

    Add files via upload

---
+++ TextForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react"
+
+
+
+export default function TextForm( props){
+
+  const handleUpClick =() => {
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+
+  const handleLowClick =() => {
+    let newText = text.toLocaleLowerCase();
+    setText(newText)
+  }
+
+  const handleOnChange =(event) => {
+    
+    setText(event.target.value)
+  }
+
+  const [text, setText] = useState("Enter your text");
+
+
+    return(
+<div>
+<h1>{props.heading}</h1>
+  <div className="mb-3">
+    <label for ="myBox" className="form-label">Example text area</label>
+    </div>
+    <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+   
+<button className="btn btn-primary" onClick={handleUpClick}> onvert to upper</button>
+<button className="btn btn-primary" onClick={handleLowClick}> onvert to upper</button>
+
+
+
+  
+</div>
+    )
+}
